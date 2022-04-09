f28cbeed8bd19f1aefdb8fe358df7b5ad62f2851
Braden Wong <bmw02002turbo@gmail.com>
2022-04-09 00:18:57 -0400

    SearchBar: create

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,5 @@
+import { Input } from "@chakra-ui/react"
+// Create a round search bar with a dropdown for options to filter with
+export function SearchBar() {
+  return <Input placeholder="Search Clubs..." variant="outline" />
+}
