ce489396f83d137cd1028d29534eb9b2d5f1fd33
Braden Wong <bmw02002turbo@gmail.com>
2022-04-09 00:45:10 -0400

    HandleChange

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,14 @@
 import { Input } from "@chakra-ui/react"
+import { useState } from "react"
 // Create a round search bar with a dropdown for options to filter with
 export function SearchBar() {
-  return <Input placeholder="Search Clubs..." variant="outline" />
+  const [value, setValue] = useState("")
+  const handleChange = (event) => setValue(event.target.value)
+  return (
+    <Input
+      placeholder="Search Clubs..."
+      variant="filled"
+      onChange={handleChange}
+    />
+  )
 }
