46046460b0ae6441c8ef55363bb931c3d75ddeb0
Braden Wong <bmw02002turbo@gmail.com>
2022-04-08 23:42:08 -0400

    Nav Items

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -267,21 +267,6 @@ interface NavItem {
 
 const NAV_ITEMS: Array<NavItem> = [
   {
-    label: "Inspiration",
-    children: [
-      {
-        label: "Explore Design Work",
-        subLabel: "Trending Design to inspire you",
-        href: "#",
-      },
-      {
-        label: "New & Noteworthy",
-        subLabel: "Up-and-coming Designers",
-        href: "#",
-      },
-    ],
-  },
-  {
     label: "Find Work",
     children: [
       {
@@ -305,4 +290,3 @@ const NAV_ITEMS: Array<NavItem> = [
     href: "#",
   },
 ]
-
