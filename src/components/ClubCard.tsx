d0798aae12086fb94b888781b72ee1e9a36688f5
Braden Wong <bmw02002turbo@gmail.com>
2022-04-09 21:03:32 -0400

    Feat: Lazy with Fallback ClubCardText

--- src/components/ClubCard.tsx
+++ src/components/ClubCard.tsx
@@ -1,53 +1,16 @@
 import {
-  Box,
   Center,
-  Heading,
-  Text,
-  Stack,
-  useColorModeValue,
-  Avatar,
-  Flex,
-  Link,
 } from '@chakra-ui/react';
-import {lazy} from 'react';
 import {Club} from '../types/Club';
+import ClubCardFull from './ClubCard/ClubCardFull';
 
-// Lazy import ClubCardText
-const ClubCardText = lazy(() => import('./ClubCard/ClubCardText'));
 
 
 
 export function ClubCardTemplate({club}: {club: Club}) {
   return (
     <Center py={6}>
-      <Box
-        bg={useColorModeValue('white', 'gray.900')}
-        boxShadow={'xl'}
-        rounded={'md'}
-        p={6}
-        height={'360px'}
-        width={'360px'}
-        overflow={'hidden'}>
-        <Box
-          height={'100px'}
-          bg={'gray.100'}
-          mt={-6}
-          mx={-6}
-          mb={3}
-          pos={'relative'}>
-        </Box>
-        <Flex justify={'center'} mt={-12} mb={4}>
-          <Avatar
-            loading='lazy'
-            size='lg'
-            src={club.logo}
-            css={{
-              border: '2px solid white',
-            }}
-          />
-        </Flex>
-        <ClubCardText club={club} />
-      </Box>
+      <ClubCardFull club={club}></ClubCardFull>
     </Center >
   );
 }
