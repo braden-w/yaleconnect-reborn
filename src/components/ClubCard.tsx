f7be22c7e34b9434751786a66fba3fcfeef49bbe
Braden Wong <bmw02002turbo@gmail.com>
2022-04-09 20:57:30 -0400

    ClubCardTemplate with image

--- src/components/ClubCard.tsx
+++ src/components/ClubCard.tsx
@@ -20,7 +20,34 @@ const ClubCardText = lazy(() => import('./ClubCard/ClubCardText'));
 export function ClubCardTemplate({club}: {club: Club}) {
   return (
     <Center py={6}>
-      <ClubCardText club={club} />
+      <Box
+        bg={useColorModeValue('white', 'gray.900')}
+        boxShadow={'xl'}
+        rounded={'md'}
+        p={6}
+        height={'360px'}
+        width={'360px'}
+        overflow={'hidden'}>
+        <Box
+          height={'100px'}
+          bg={'gray.100'}
+          mt={-6}
+          mx={-6}
+          mb={3}
+          pos={'relative'}>
+        </Box>
+        <Flex justify={'center'} mt={-12} mb={4}>
+          <Avatar
+            loading='lazy'
+            size='lg'
+            src={club.logo}
+            css={{
+              border: '2px solid white',
+            }}
+          />
+        </Flex>
+        <ClubCardText club={club} />
+      </Box>
     </Center >
   );
 }
