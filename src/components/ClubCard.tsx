0c6e85d1159cfcc9066b7a393793ba08ad72a861
Braden Wong <bmw02002turbo@gmail.com>
2022-04-09 21:07:45 -0400

    ClubCard

--- src/components/ClubCard.tsx
+++ src/components/ClubCard.tsx
@@ -1,16 +1,22 @@
 import {
   Center,
 } from '@chakra-ui/react';
+import {lazy, Suspense} from 'react';
 import {Club} from '../types/Club';
-import ClubCardFull from './ClubCard/ClubCardFull';
+import ClubCardText from './ClubCard/ClubCardText';
 
 
+// Lazy import ClubCardFull
+const ClubCardFull = lazy(() => import('./ClubCard/ClubCardFull'));
 
 
 export function ClubCardTemplate({club}: {club: Club}) {
   return (
     <Center py={6}>
-      <ClubCardFull club={club}></ClubCardFull>
+      <Suspense fallback={ClubCardText}>
+        <ClubCardFull club={club}></ClubCardFull>
+      </Suspense>
+
     </Center >
   );
 }
