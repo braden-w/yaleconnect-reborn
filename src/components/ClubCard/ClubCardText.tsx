f2f1cadb93aa8c5a426f6282586251ef8e6cab58
Braden Wong <bmw02002turbo@gmail.com>
2022-04-10 10:59:17 -0400

    Categories

--- src/components/ClubCard/ClubCardText.tsx
+++ src/components/ClubCard/ClubCardText.tsx
@@ -45,16 +45,18 @@ export default function ClubCardText({club}: {club: Club}) {
 		<Modal isOpen={isOpen} onClose={onClose}>
 			<ModalOverlay />
 			<ModalContent>
-				<ModalHeader>{club.name}</ModalHeader>
+				<ModalHeader>{club.name}
+					<Text color='gray.500' fontWeight={'normal'} fontSize='lg'>{club.type}</Text>
+				</ModalHeader>
 				<ModalCloseButton />
 				<ModalBody>
-					<Text>
-						{club.categoryParsed && <>Club Category: {club.categoryParsed}</>}
-					</Text>
 					{/* <Text>
 						{club.categories && club.categories.length > 1 && <>Other Categories: {club.categories.map(({category}) => category).join()}</>}
 					</Text> */}
-					{club.mission}
+					<Text>{club.mission}</Text>
+					<Text color={'gray.500'} noOfLines={[1, 2]}>
+						{club.categoryParsed && <>Club Category: {club.categoryParsed}</>}
+					</Text>
 				</ModalBody>
 				<ModalFooter>
 					<Button colorScheme='blue' mr={3} onClick={onClose}>
