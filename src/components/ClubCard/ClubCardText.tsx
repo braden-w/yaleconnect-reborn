0e5e15ad2f8c095fc52f741788d3bb2d34a893ac
Braden Wong <bmw02002turbo@gmail.com>
2022-04-09 20:59:21 -0400

    Club prop types

--- src/components/ClubCard/ClubCardText.tsx
+++ src/components/ClubCard/ClubCardText.tsx
@@ -1,13 +1,14 @@
 import {Text, Stack, Heading, useColorModeValue, Box} from "@chakra-ui/react";
 import Link from "next/link";
+import {Club} from "../../types/Club";
 
-export default function ClubCardText(props) {
+export default function ClubCardText({club}: {club: Club}) {
 	return (<Stack>
 		<Heading color={useColorModeValue('gray.700', 'white')} textTransform={'uppercase'} fontFamily={'body'} fontSize={'2l'} fontWeight={800} letterSpacing={1.1} noOfLines={[2, 3]}>
-			{props.club.name}
+			{club.name}
 		</Heading>
 		<Text color={'gray.500'} noOfLines={[1, 2]}>
-			{props.club.type}
+			{club.type}
 		</Text>
 		{
 			/* Insert a horizontal divider */
@@ -16,14 +17,14 @@ export default function ClubCardText(props) {
 		{
 			/* External link to club.website */
 		}
-		<Link href={props.club.website}>
+		<Link href={club.website}>
 			<Text color={'blue.500'}>Website</Text>
 		</Link>
 		{
 			/* Insert text for club.mission.*/
 		}
 		<Text color={'gray.500'} fontSize='sm' noOfLines={[1, 2]}>
-			{props.club.mission}
+			{club.mission}
 		</Text>
 
 	</Stack>);
