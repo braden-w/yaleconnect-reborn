86af3804e8f663f64c9f6f21d52067678adf5c91
Braden Wong <bmw02002turbo@gmail.com>
2022-04-10 10:45:48 -0400

    Club Categories

--- src/components/ClubCard/ClubCardText.tsx
+++ src/components/ClubCard/ClubCardText.tsx
@@ -1,22 +1,24 @@
-import {Text, 
-		Stack, 
-		Heading, 
-		useColorModeValue, 
-		Box, 
-		Modal,
-		ModalOverlay,
-		ModalContent,
-		ModalHeader,
-		ModalFooter,
-		ModalBody,
-		ModalCloseButton,
-		useDisclosure,
-		Button} from "@chakra-ui/react";
+import {
+	Text,
+	Stack,
+	Heading,
+	useColorModeValue,
+	Box,
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalFooter,
+	ModalBody,
+	ModalCloseButton,
+	useDisclosure,
+	Button
+} from "@chakra-ui/react";
 import Link from "next/link";
 import {Club} from "../../types/Club";
 
 export default function ClubCardText({club}: {club: Club}) {
-	const { isOpen, onOpen, onClose } = useDisclosure();
+	const {isOpen, onOpen, onClose} = useDisclosure();
 	return (<Stack>
 		<Heading color={useColorModeValue('gray.700', 'white')} textTransform={'uppercase'} fontFamily={'body'} fontSize={'2l'} fontWeight={800} letterSpacing={1.1} noOfLines={[2, 3]}>
 			{club.name}
@@ -41,24 +43,27 @@ export default function ClubCardText({club}: {club: Club}) {
 		<Button onClick={onOpen}>Read More</Button>
 
 		<Modal isOpen={isOpen} onClose={onClose}>
-	  		<ModalOverlay />
-	  		<ModalContent>
+			<ModalOverlay />
+			<ModalContent>
 				<ModalHeader>{club.name}</ModalHeader>
 				<ModalCloseButton />
-			<ModalBody>
-				<Text>
-					Club Category: {club.categoryParsed}
-				</Text>
-		  		{club.mission}
-			</ModalBody>
-			<ModalFooter>
-		  		<Button colorScheme='blue' mr={3} onClick={onClose}>
-					Close
-		  		</Button>
-			</ModalFooter>
-	  	</ModalContent>
-	</Modal>
+				<ModalBody>
+					<Text>
+						{club.categoryParsed && <>Club Category: {club.categoryParsed}</>}
+					</Text>
+					{/* <Text>
+						{club.categories && club.categories.length > 1 && <>Other Categories: {club.categories.map(({category}) => category).join()}</>}
+					</Text> */}
+					{club.mission}
+				</ModalBody>
+				<ModalFooter>
+					<Button colorScheme='blue' mr={3} onClick={onClose}>
+						Close
+					</Button>
+				</ModalFooter>
+			</ModalContent>
+		</Modal>
 	</Stack>
-	
+
 	);
 }
